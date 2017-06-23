55b056ee97c4c3f5b69fefc6b6118de28a7c9a45
asinkxcoswt <asinkxcoswt@gmail.com>
2017-06-23 20:09:55 +0700

    add babel support

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,39 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
+const debug = process.env.NODE_ENV !== "production";
+const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
-  context: __dirname,
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/scripts.js",
-  output: {
-    path: __dirname + "/js",
-    filename: "scripts.min.js"
-  },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+    context: __dirname,
+    devtool: debug ? "inline-sourcemap" : null,
+    entry: "./js/scripts.jsx",
+    output: {
+        path: __dirname + "/js",
+        filename: "scripts.min.js"
+    },
+    plugins: debug ? [] : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    ],
+    module: {
+        loaders: [
+            {
+                loader: "babel-loader",
+
+                // Skip any files outside of your project's `src` directory
+                include: [
+                    path.resolve(__dirname, "js"),
+                ],
+
+                // Only run `.js` and `.jsx` files through Babel
+                test: /\.jsx?$/,
+
+                // Options to configure babel with
+                query: {
+                    plugins: ['transform-runtime'],
+                    presets: ['es2015', 'stage-0', 'react'],
+                }
+            },
+        ]
+    }
 };
